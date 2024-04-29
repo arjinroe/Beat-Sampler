a9e32a1a56483cdd39a5a6b893cc6a2534bb52df
Arjin Roe <arjinroe@gmail.com>
2024-04-28 23:23:11 -0400

    v0.5
    Changed code so samples can be triggered repeatedly, added 7 additional samples. Next step will be adding MIDI support.

--- samplePlayback.js
+++ samplePlayback.js
@@ -1,78 +1,129 @@
+//create buttons
+const playKick1 = document.querySelector(".play");
+const playKick2 = document.querySelector(".play2");
+const playSnare1 = document.querySelector(".play3");
+const playSnare2 = document.querySelector(".play4");
+const playHatsClosed = document.querySelector(".play5");
+const playHatsOpen = document.querySelector(".play6");
+const playToms1 = document.querySelector(".play7");
+const playToms2 = document.querySelector(".play8");
+const playToms3 = document.querySelector(".play9");
+
 //create audio context
-const superAudioContext = new (AudioContext || webkit.AudioContext)();
+const audioContext = new AudioContext();
 
-// create AUDIO NODES
-const source = superAudioContext.createBufferSource();
-const myGain = superAudioContext.createGain();
-const myFilter = superAudioContext.createBiquadFilter();
+//link to audio files
+const kick1 = new Audio ("./kick.wav");
+const kick2 = new Audio ("./kick2.wav");
+const snare1 = new Audio ("./snare.wav");
+const snare2 = new Audio ("./snare2.wav");
+const hatClosed = new Audio ("./hh-closed.wav");
+const hatOpen = new Audio ("./hh-open.wav");
+const tom1 = new Audio ("./tom1.wav");
+const tom2 = new Audio ("./tom2.wav");
+const tom3 = new Audio ("./tom3.wav");
+
+const source = audioContext.createMediaElementSource(kick1);
+const source2 = audioContext.createMediaElementSource(kick2);
+const source3 = audioContext.createMediaElementSource(snare1);
+const source4 = audioContext.createMediaElementSource(snare2);
+const source5 = audioContext.createMediaElementSource(hatClosed);
+const source6 = audioContext.createMediaElementSource(hatOpen);
+const source7 = audioContext.createMediaElementSource(tom1);
+const source8 = audioContext.createMediaElementSource(tom2);
+const source9 = audioContext.createMediaElementSource(tom3);
+
+source.connect(audioContext.destination);
+source2.connect(audioContext.destination);
+source3.connect(audioContext.destination);
+source4.connect(audioContext.destination);
+source5.connect(audioContext.destination);
+source6.connect(audioContext.destination);
+source7.connect(audioContext.destination);
+source8.connect(audioContext.destination);
+source9.connect(audioContext.destination);
 
 
-//connect AUDIO NODES
-source.connect(myFilter);
-myFilter.connect(myGain);
-myGain.connect(superAudioContext.destination);
-
-//initial values
-myGain.gain.value = 1.0;
-myFilter.type = "peaking"; //filter type
-myFilter.frequency.setValueAtTime(1000, superAudioContext.currentTime); //cutoff freq
-myFilter.Q.setValueAtTime(10, superAudioContext.currentTime);
-myFilter.gain.setValueAtTime(25, superAudioContext.currentTime);
-
-
-//let's import the sample...
-    //but there's a problem, these audio files are LARGE and take time to load...
-    //so we'll build functions that use the AWAIT tag...
-
-//get raw data from audio file...
-const getAudioFile = async function(url) { //make an async function if you want it to start before anything else happens...
-    const response = await fetch(url); //fetch grabs the file...
-    const arrayBuffer = await response.arrayBuffer(); //arrayBuffer grabs the binary code...
-    return arrayBuffer;
-};
-
-//decode raw data into audio data...
-const decodeAudioData = async function(audioContext, dataBuffer){
-    //decoding raw data into audio data.
-    const audioBuffer = await audioContext.decodeAudioData(dataBuffer);
-    return audioBuffer;
-};
-
-//load into buffer and play...
-const playAudio = function (audioContext, audioBuff){
-    //create a BufferSource node...
-    source.buffer = audioBuff;
-    source.start();
-};
-//now let's call these functions...
-const fileData = await getAudioFile("./kick.wav");
-const fileData2 = await getAudioFile("./snare.wav");
-
-const mySpecialBuffer = await decodeAudioData(superAudioContext, fileData);
-console.log(mySpecialBuffer);
-const mySpecialBuffer2 = await decodeAudioData(superAudioContext, fileData2);
-console.log(mySpecialBuffer2);
-
-mySpecialBuffer.getChannelData(0).forEach(function (s) {
-    s*2;
+//event listeners
+playKick1.addEventListener("click", () => {
+    if (audioContext.state === "suspended") {
+        audioContext.resume();
+        console.log(audioContext);
+    };
+    kick1.play();
 });
 
-document.getElementById("play").addEventListener("click", function () {
-    superAudioContext.resume();
-    playAudio(superAudioContext, mySpecialBuffer);
+playKick2.addEventListener("click", () => {
+    kick2.play();
 });
 
-document.getElementById("play2").addEventListener("click", function () {
-    superAudioContext.resume();
-    playAudio(superAudioContext, mySpecialBuffer2);
+playSnare1.addEventListener("click", () => {
+    snare1.play();
 });
 
-//note: the following stops the audiocontext permanently
-    //if you want to not do this, then you'll want to make a
-    //gain node and simply turn that down
-document.getElementById("stop").addEventListener("click", function () {
-    source.stop();
+playSnare2.addEventListener("click", () => {
+    snare2.play();
 });
+
+playHatsClosed.addEventListener("click", () => {
+    hatClosed.play();
+});
+
+playHatsOpen.addEventListener("click", () => {
+    hatOpen.play();
+});
+
+playToms1.addEventListener("click", () => {
+    tom1.play();
+});
+
+playToms2.addEventListener("click", () => {
+    tom2.play();
+});
+
+playToms3.addEventListener("click", () => {
+    tom3.play();
+});
+
+
+
+
+
+
+
+
+
+
+function initDevices(midi) {
+    // Reset.
+    midiIn = [];
+    midiOut = [];
+    
+    // MIDI devices that send you data.
+    const inputs = midi.inputs.values();
+    for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
+      midiIn.push(input.value);
+    }
+    
+    // MIDI devices that you send data to.
+    const outputs = midi.outputs.values();
+    for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
+      midiOut.push(output.value);
+    }
+    
+    displayDevices();
+    startListening();
+  }
+  
+  
+  // Start listening to MIDI messages.
+  function startListening() {
+    for (const input of midiIn) {
+      input.addEventListener('midimessage', midiMessageReceived);
+    }
+  }
+
+
 
 
 
