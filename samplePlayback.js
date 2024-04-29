8adffdd1efed610b1ab0a5af2f4de3d817cc7519
Arjin Roe <arjinroe@gmail.com>
2024-04-29 00:19:32 -0400

    ALPHA 0.7
    Changelog:
    Added MIDI functionality with generic MIDI keyboard and mappings.

--- samplePlayback.js
+++ samplePlayback.js
@@ -1,4 +1,87 @@
-//create buttons
+// Enable WebMidi API and handle any errors if it fails to enable.
+// This is necessary to work with MIDI devices in the web browser.
+await WebMidi.enable();
+
+//initialize variables for MIDI
+let myInput = WebMidi.inputs[0];
+let myOutput = WebMidi.outputs[0];
+
+
+// Get the dropdown elements from the HTML document by their IDs.
+// These dropdowns will be used to display the MIDI input devices available.
+let dropIns = document.getElementById("dropdown-ins");
+
+// For each MIDI input device detected, add an option to the input devices dropdown.
+// This loop iterates over all detected input devices, adding them to the dropdown.
+WebMidi.inputs.forEach(function (input, num) {
+    dropIns.innerHTML += `<option value=${num}>${input.name}</option>`;
+  });
+
+
+// Add an event listener for the 'change' event on the input devices dropdown.
+// This allows the script to react when the user selects a different MIDI input device.
+dropIns.addEventListener("change", function () {
+    // Before changing the input device, remove any existing event listeners
+    // to prevent them from being called after the device has been changed.
+    if (myInput.hasListener("noteon")) {
+      myInput.removeListener("noteon");
+    }
+    if (myInput.hasListener("noteoff")) {
+      myInput.removeListener("noteoff");
+    }
+  
+    // Change the input device based on the user's selection in the dropdown.
+    myInput = WebMidi.inputs[dropIns.value];
+  
+    // After changing the input device, add new listeners for 'noteon' and 'noteoff' events.
+    // These listeners will handle MIDI note on (key press) and note off (key release) messages.
+    myInput.addListener("noteon", function (someMIDI) {
+      // When a note on event is received, send a note on message to the output device.
+      // This can trigger a sound or action on the MIDI output device.
+      myOutput.sendNoteOn(someMIDI.note);
+      console.log(someMIDI.note.number);
+      if (someMIDI.note.number == 60) {
+        kick1.play();
+      }
+      else if (someMIDI.note.number == 62) {
+        kick2.play();
+      }
+      else if (someMIDI.note.number == 64) {
+        snare1.play();
+      }
+      else if (someMIDI.note.number == 65) {
+        snare2.play();
+      }
+      else if (someMIDI.note.number == 67) {
+        hatClosed.play();
+      }
+      else if (someMIDI.note.number == 69) {
+        hatOpen.play();
+      }
+      else if (someMIDI.note.number == 71) {
+        tom1.play();
+      }
+      else if (someMIDI.note.number == 72) {
+        tom2.play();
+      }
+      else if (someMIDI.note.number == 74) {
+        tom3.play();
+      }
+    });
+  
+  
+  
+    myInput.addListener("noteoff", function (someMIDI) {
+      // Similarly, when a note off event is received, send a note off message to the output device.
+      // This signals the end of a note being played.
+      myOutput.sendNoteOff(someMIDI.note);
+    });
+  });
+
+
+
+
+//link to HTML buttons in JS
 const playKick1 = document.querySelector(".play");
 const playKick2 = document.querySelector(".play2");
 const playSnare1 = document.querySelector(".play3");
@@ -42,7 +125,6 @@ source6.connect(audioContext.destination);
 source7.connect(audioContext.destination);
 source8.connect(audioContext.destination);
 source9.connect(audioContext.destination);
-
 
 //event listeners
 playKick1.addEventListener("click", () => {
@@ -116,44 +198,6 @@ playToms3.addEventListener("click", () => {
     };
     tom3.play();
 });
-
-
-
-
-
-
-
-
-
-
-function initDevices(midi) {
-    // Reset.
-    midiIn = [];
-    midiOut = [];
-    
-    // MIDI devices that send you data.
-    const inputs = midi.inputs.values();
-    for (let input = inputs.next(); input && !input.done; input = inputs.next()) {
-      midiIn.push(input.value);
-    }
-    
-    // MIDI devices that you send data to.
-    const outputs = midi.outputs.values();
-    for (let output = outputs.next(); output && !output.done; output = outputs.next()) {
-      midiOut.push(output.value);
-    }
-    
-    displayDevices();
-    startListening();
-  }
-  
-  
-  // Start listening to MIDI messages.
-  function startListening() {
-    for (const input of midiIn) {
-      input.addEventListener('midimessage', midiMessageReceived);
-    }
-  }
 
 
 
