cf3b089640c18842d74719993a48889c4e41a0fe
Arjin Roe <arjinroe@gmail.com>
2024-05-02 17:10:46 -0400

    v1.0
    CHANGELOG:
    - Added better GUI
    - Added MIDI mapping selection between Nektar Aruba & Generic MIDI controller
    - Added 18 additional samples in two additional banks for a total of four banks of 9 samples each
    - Added master volume slider
    
    OTHER NOTES:
    - Completed rigorous testing, testing document is included within this commit
    
    FINAL NOTES:
    - This is the final version, and the project will now be publically deployed.

--- samplePlayback.js
+++ samplePlayback.js
@@ -11,6 +11,23 @@ let myOutput = WebMidi.outputs[0];
 // These dropdowns will be used to display the MIDI input devices available.
 let dropIns = document.getElementById("dropdown-ins");
 let bankSelect = document.getElementById("bankSelect");
+let mapSelect = document.getElementById("mapSelect");
+
+
+//initialize MIDI Mapping Values
+let midiVal = [36,37,38,40,41,42,44,45,46];
+
+mapSelect.addEventListener("change", function (){
+  if (mapSelect.value == "Nektar Aruba") {
+    midiVal = [36,37,38,40,41,42,44,45,46];
+  }
+  else if (mapSelect.value == "Generic MIDI") {
+    midiVal = [60,62,64,65,67,69,71,72,74];
+  }
+  return midiVal;
+});
+
+
 
 // For each MIDI input device detected, add an option to the input devices dropdown.
 // This loop iterates over all detected input devices, adding them to the dropdown.
@@ -36,72 +53,139 @@ dropIns.addEventListener("change", function () {
   
     // After changing the input device, add new listeners for 'noteon' and 'noteoff' events.
     // These listeners will handle MIDI note on (key press) and note off (key release) messages.
+
+    // This listener function takes in the MIDI input and then depending on the
+    // MIDI value it triggers different samples
     myInput.addListener("noteon", function (someMIDI) {
       // When a note on event is received, send a note on message to the output device.
       // This can trigger a sound or action on the MIDI output device.
       myOutput.sendNoteOn(someMIDI.note);
-      console.log(someMIDI.note.number);
-      if (someMIDI.note.number == 36 && bankSelect.value == "bankA") {
+      // console.log(someMIDI.note.number);
+      
+      //KICK 1
+      if (someMIDI.note.number == midiVal[0] && bankSelect.value == "bankA") {
         aKick1.play();
       }
-      else if (someMIDI.note.number == 36 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[0] && bankSelect.value == "bankB") {
         bKick1.play();
       }
+      else if (someMIDI.note.number == midiVal[0] && bankSelect.value == "bankC") {
+        cKick1.play();
+      }
+      else if (someMIDI.note.number == midiVal[0] && bankSelect.value == "bankD") {
+        dKick1.play();
+      }
 
-      else if (someMIDI.note.number == 37 && bankSelect.value == "bankA") {
+      //KICK 2
+      else if (someMIDI.note.number == midiVal[1] && bankSelect.value == "bankA") {
         aKick2.play();
       }
-      else if (someMIDI.note.number == 37 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[1] && bankSelect.value == "bankB") {
         bKick2.play();
       }
+      else if (someMIDI.note.number == midiVal[1] && bankSelect.value == "bankC") {
+        cKick2.play();
+      }
+      else if (someMIDI.note.number == midiVal[1] && bankSelect.value == "bankD") {
+        dKick2.play();
+      }
 
-      else if (someMIDI.note.number == 38 && bankSelect.value == "bankA") {
+      //SNARE 1
+      else if (someMIDI.note.number == midiVal[2] && bankSelect.value == "bankA") {
         aSnare1.play();
       }
-      else if (someMIDI.note.number == 38 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[2] && bankSelect.value == "bankB") {
         bSnare1.play();
       }
+      else if (someMIDI.note.number == midiVal[2] && bankSelect.value == "bankC") {
+        cSnare1.play();
+      }
+      else if (someMIDI.note.number == midiVal[2] && bankSelect.value == "bankD") {
+        dSnare1.play();
+      }
 
-      else if (someMIDI.note.number == 39 && bankSelect.value == "bankA") {
+      //SNARE 2
+      else if (someMIDI.note.number == midiVal[3] && bankSelect.value == "bankA") {
         aSnare2.play();
       }
-      else if (someMIDI.note.number == 39 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[3] && bankSelect.value == "bankB") {
         bSnare2.play();
       }
+      else if (someMIDI.note.number == midiVal[3] && bankSelect.value == "bankC") {
+        cSnare2.play();
+      }
+      else if (someMIDI.note.number == midiVal[3] && bankSelect.value == "bankD") {
+        dSnare2.play();
+      }
 
-      else if (someMIDI.note.number == 40 && bankSelect.value == "bankA") {
+      //HI-HAT CLOSED
+      else if (someMIDI.note.number == midiVal[4] && bankSelect.value == "bankA") {
         aHatClosed.play();
       }
-      else if (someMIDI.note.number == 40 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[4] && bankSelect.value == "bankB") {
         bHatClosed.play();
       }
+      else if (someMIDI.note.number == midiVal[4] && bankSelect.value == "bankC") {
+        cHatClosed.play();
+      }
+      else if (someMIDI.note.number == midiVal[4] && bankSelect.value == "bankD") {
+        dHatClosed.play();
+      }
 
-      else if (someMIDI.note.number == 41 && bankSelect.value == "bankA") {
+      //HI-HAT OPEN
+      else if (someMIDI.note.number == midiVal[5] && bankSelect.value == "bankA") {
         aHatOpen.play();
       }
-      else if (someMIDI.note.number == 41 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[5] && bankSelect.value == "bankB") {
         bHatOpen.play();
       }
+      else if (someMIDI.note.number == midiVal[5] && bankSelect.value == "bankC") {
+        cHatOpen.play();
+      }
+      else if (someMIDI.note.number == midiVal[5] && bankSelect.value == "bankD") {
+        dHatOpen.play();
+      }
 
-      else if (someMIDI.note.number == 42 && bankSelect.value == "bankA") {
+      //TOM 1
+      else if (someMIDI.note.number == midiVal[6] && bankSelect.value == "bankA") {
         aTom1.play();
       }
-      else if (someMIDI.note.number == 42 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[6] && bankSelect.value == "bankB") {
         bTom1.play();
       }
+      else if (someMIDI.note.number == midiVal[6] && bankSelect.value == "bankC") {
+        cTom1.play();
+      }
+      else if (someMIDI.note.number == midiVal[6] && bankSelect.value == "bankD") {
+        dTom1.play();
+      }
 
-      else if (someMIDI.note.number == 43 && bankSelect.value == "bankA") {
+      //TOM 2
+      else if (someMIDI.note.number == midiVal[7] && bankSelect.value == "bankA") {
         aTom2.play();
       }
-      else if (someMIDI.note.number == 43 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[7] && bankSelect.value == "bankB") {
         bTom2.play();
       }
+      else if (someMIDI.note.number == midiVal[7] && bankSelect.value == "bankC") {
+        cTom2.play();
+      }
+      else if (someMIDI.note.number == midiVal[7] && bankSelect.value == "bankD") {
+        dTom2.play();
+      }
 
-      else if (someMIDI.note.number == 44 && bankSelect.value == "bankA") {
+      //TOM 3
+      else if (someMIDI.note.number == midiVal[8] && bankSelect.value == "bankA") {
         aTom3.play();
       }
-      else if (someMIDI.note.number == 44 && bankSelect.value == "bankB") {
+      else if (someMIDI.note.number == midiVal[8] && bankSelect.value == "bankB") {
         bTom3.play();
+      }
+      else if (someMIDI.note.number == midiVal[8] && bankSelect.value == "bankC") {
+        cTom3.play();
+      }
+      else if (someMIDI.note.number == midiVal[8] && bankSelect.value == "bankD") {
+        dTom3.play();
       }
     });
   
@@ -115,9 +199,19 @@ dropIns.addEventListener("change", function () {
   });
 
   bankSelect.addEventListener("change", function () {
-    console.log(bankSelect.value);
+    // console.log(bankSelect.value);
     return bankSelect;
   });
+
+  const updateGain = function() {
+    // audioContext.resume()
+    let sliderVal = parseFloat(gainControl.value)
+    document.getElementById("gainDisplay").innerText = `${sliderVal} dBFS`
+    let linAmp = 10**(sliderVal/20)
+    // console.log(sliderVal, linAmp)
+    gainNode.gain.setValueAtTime(linAmp, audioContext.currentTime);
+    // console.log(gainNode.gain.value)
+}
 
 
 //link to HTML buttons in JS
@@ -136,17 +230,15 @@ const playToms3 = document.querySelector(".play9");
 //create audio context
 const audioContext = new AudioContext();
 
-// let kick1;
-// let kick2;
-// let snare1;
-// let snare2;
-// let hatClosed;
-// let hatOpen;
-// let tom1;
-// let tom2;
-// let tom3;
+const gainControl = document.getElementById("gainSlider");
 
-//link to audio files
+const gainNode = audioContext.createGain();
+gainNode.connect(audioContext.destination);
+gainNode.gain.value = 0. //linear amplitude
+
+//link to audio files with a being BANK A and b being BANK B
+
+// BANK A
 let aKick1 = new Audio ("./kick.wav");
 let aKick2 = new Audio ("./kick2.wav");
 let aSnare1 = new Audio ("./snare.wav");
@@ -157,6 +249,7 @@ let aTom1 = new Audio ("./tom1.wav");
 let aTom2 = new Audio ("./tom2.wav");
 let aTom3 = new Audio ("./tom3.wav");
 
+// BANK B
 let bKick1 = new Audio ("./b-kick.wav");
 let bKick2 = new Audio ("./b-kick2.wav");
 let bSnare1 = new Audio ("./b-snare1.wav");
@@ -167,13 +260,30 @@ let bTom1 = new Audio ("./b-tom1.wav");
 let bTom2 = new Audio ("./b-tom2.wav");
 let bTom3 = new Audio ("./b-tom3.wav");
 
+// BANK C
+let cKick1 = new Audio ("./c-kick1.wav");
+let cKick2 = new Audio ("./c-kick2.wav");
+let cSnare1 = new Audio ("./c-snare1.wav");
+let cSnare2 = new Audio ("./c-snare2.wav");
+let cHatClosed = new Audio ("./c-hh-closed.wav");
+let cHatOpen = new Audio ("./c-hh-open.wav");
+let cTom1 = new Audio ("./c-tom1.wav");
+let cTom2 = new Audio ("./c-tom2.wav");
+let cTom3 = new Audio ("./c-tom3.wav");
+
+// BANK D
+let dKick1 = new Audio ("./d-kick1.wav");
+let dKick2 = new Audio ("./d-kick2.wav");
+let dSnare1 = new Audio ("./d-snare1.wav");
+let dSnare2 = new Audio ("./d-snare2.wav");
+let dHatClosed = new Audio ("./d-hh-closed.wav");
+let dHatOpen = new Audio ("./d-hh-open.wav");
+let dTom1 = new Audio ("./d-tom1.wav");
+let dTom2 = new Audio ("./d-tom2.wav");
+let dTom3 = new Audio ("./d-tom3.wav");
 
 
-// selectBankA.addEventListener("click", bankAClick);
-// selectBankB.addEventListener("click", bankBClick);
-
-
-
+// creating SOURCE MEDIA from SAMPLES
 const source = audioContext.createMediaElementSource(aKick1);
 const source2 = audioContext.createMediaElementSource(aKick2);
 const source3 = audioContext.createMediaElementSource(aSnare1);
@@ -194,31 +304,75 @@ const source16 = audioContext.createMediaElementSource(bTom1);
 const source17 = audioContext.createMediaElementSource(bTom2);
 const source18 = audioContext.createMediaElementSource(bTom3);
 
-source.connect(audioContext.destination);
-source2.connect(audioContext.destination);
-source3.connect(audioContext.destination);
-source4.connect(audioContext.destination);
-source5.connect(audioContext.destination);
-source6.connect(audioContext.destination);
-source7.connect(audioContext.destination);
-source8.connect(audioContext.destination);
-source9.connect(audioContext.destination);
+const source19 = audioContext.createMediaElementSource(cKick1);
+const source20 = audioContext.createMediaElementSource(cKick2);
+const source21 = audioContext.createMediaElementSource(cSnare1);
+const source22 = audioContext.createMediaElementSource(cSnare2);
+const source23 = audioContext.createMediaElementSource(cHatClosed);
+const source24 = audioContext.createMediaElementSource(cHatOpen);
+const source25 = audioContext.createMediaElementSource(cTom1);
+const source26 = audioContext.createMediaElementSource(cTom2);
+const source27 = audioContext.createMediaElementSource(cTom3);
 
-source10.connect(audioContext.destination);
-source11.connect(audioContext.destination);
-source12.connect(audioContext.destination);
-source13.connect(audioContext.destination);
-source14.connect(audioContext.destination);
-source15.connect(audioContext.destination);
-source16.connect(audioContext.destination);
-source17.connect(audioContext.destination);
-source18.connect(audioContext.destination);
+const source28 = audioContext.createMediaElementSource(dKick1);
+const source29 = audioContext.createMediaElementSource(dKick2);
+const source30 = audioContext.createMediaElementSource(dSnare1);
+const source31 = audioContext.createMediaElementSource(dSnare2);
+const source32 = audioContext.createMediaElementSource(dHatClosed);
+const source33 = audioContext.createMediaElementSource(dHatOpen);
+const source34 = audioContext.createMediaElementSource(dTom1);
+const source35 = audioContext.createMediaElementSource(dTom2);
+const source36 = audioContext.createMediaElementSource(dTom3);
 
-//event listeners
+
+// Connecting SOURCE to DESTINATION
+source.connect(gainNode);
+source2.connect(gainNode);
+source3.connect(gainNode);
+source4.connect(gainNode);
+source5.connect(gainNode);
+source6.connect(gainNode);
+source7.connect(gainNode);
+source8.connect(gainNode);
+source9.connect(gainNode);
+
+source10.connect(gainNode);
+source11.connect(gainNode);
+source12.connect(gainNode);
+source13.connect(gainNode);
+source14.connect(gainNode);
+source15.connect(gainNode);
+source16.connect(gainNode);
+source17.connect(gainNode);
+source18.connect(gainNode);
+
+source19.connect(gainNode);
+source20.connect(gainNode);
+source21.connect(gainNode);
+source22.connect(gainNode);
+source23.connect(gainNode);
+source24.connect(gainNode);
+source25.connect(gainNode);
+source26.connect(gainNode);
+source27.connect(gainNode);
+
+source28.connect(gainNode);
+source29.connect(gainNode);
+source30.connect(gainNode);
+source31.connect(gainNode);
+source32.connect(gainNode);
+source33.connect(gainNode);
+source34.connect(gainNode);
+source35.connect(gainNode);
+source36.connect(gainNode);
+
+gainNode.connect(audioContext.destination);
+
+//EVENT LISTENERS for OnScreen Buttons
 playKick1.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aKick1.play();
@@ -226,13 +380,18 @@ playKick1.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bKick1.play();
     }
-    
+    else if (bankSelect.value == "bankC") {
+      cKick1.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      dKick1.play();
+    }
 });
 
 playKick2.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aKick2.play();
@@ -240,12 +399,18 @@ playKick2.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bKick2.play();
     }
+    else if (bankSelect.value == "bankC") {
+      cKick2.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      dKick2.play();
+    }
 });
 
 playSnare1.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aSnare1.play();
@@ -253,12 +418,18 @@ playSnare1.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bSnare1.play();
     }
+    else if (bankSelect.value == "bankC") {
+      cSnare1.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      dSnare1.play();
+    }
 });
 
 playSnare2.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aSnare2.play();
@@ -266,12 +437,18 @@ playSnare2.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bSnare2.play();
     }
+    else if (bankSelect.value == "bankC") {
+      cSnare2.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      dSnare2.play();
+    }
 });
 
 playHatsClosed.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aHatClosed.play();
@@ -279,12 +456,18 @@ playHatsClosed.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bHatClosed.play();
     }
+    else if (bankSelect.value == "bankC") {
+      cHatClosed.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      cHatClosed.play();
+    }
 });
 
 playHatsOpen.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aHatOpen.play();
@@ -292,12 +475,18 @@ playHatsOpen.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bHatOpen.play();
     }
+    else if (bankSelect.value == "bankC") {
+      cHatOpen.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      dHatOpen.play();
+    }
 });
 
 playToms1.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aTom1.play();
@@ -305,12 +494,18 @@ playToms1.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bTom1.play();
     }
+    else if (bankSelect.value == "bankC") {
+      cTom1.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      dTom1.play();
+    }
 });
 
 playToms2.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aTom2.play();
@@ -318,12 +513,18 @@ playToms2.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bTom2.play();
     }
+    else if (bankSelect.value == "bankC") {
+      cTom2.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      dTom2.play();
+    }
 });
 
 playToms3.addEventListener("click", () => {
     if (audioContext.state === "suspended") {
         audioContext.resume();
-        console.log(audioContext);
+        // console.log(audioContext);
     };
     if (bankSelect.value == "bankA") {
       aTom3.play();
@@ -331,9 +532,15 @@ playToms3.addEventListener("click", () => {
     else if (bankSelect.value == "bankB") {
       bTom3.play();
     }
+    else if (bankSelect.value == "bankC") {
+      cTom3.play();
+    }
+    else if (bankSelect.value == "bankD") {
+      dTom3.play();
+    }
 });
 
 
-
+gainControl.addEventListener("input", updateGain)
 
 
